f13a02616c4a30e33c7c4c02c18b2cbdc3b5c9a5
qeii <103341061+qeii@users.noreply.github.com>
2025-02-22 11:07:55 +0700

    Update position resetButton

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ document.getElementById('getThumbnail').addEventListener('click', function () {
   }
 });
 
+document.getElementById('resetButton').addEventListener('click', function () {
+  document.getElementById('videoUrl').value = '';
+  document.getElementById('thumbnailContainer').style.display = 'none';
+  document.getElementById('thumbnailImage').src = '';
+  document.getElementById('downloadLink').href = '#';
+});
+
 function extractVideoId(url) {
   const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
   const match = url.match(regExp);
   return match && match[2].length === 11 ? match[2] : null;
 }
-
-document.getElementById('resetButton').addEventListener('click', function () {
-  document.getElementById('videoUrl').value = ''; // Clear input
-  document.getElementById('thumbnailContainer').style.display = 'none'; // Hide result
-  document.getElementById('thumbnailImage').src = ''; // Reset thumbnail image
-  document.getElementById('downloadLink').href = '#'; // Reset download link
-});
